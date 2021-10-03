806c37bfb732808d3e52131c4143d06c5a5fe9bf
Narcissa Wright <49114816+narcissawright@users.noreply.github.com>
2021-10-03 07:48:45 -0500

    mint_token_mainnet.sh
    builds the tx for minting coins and sends to the winneraddr.

---
+++ mint_token_mainnet.sh
@@ -0,0 +1,63 @@
+# You need to edit the mintamount
+# the winneraddr
+# and if used, the metadata.json
+# and then you can run this script to mint.
+
+mintamount="20"
+winneraddr="addr1qyeut7y949w2wqh49stkr3g5leljdxngrpdmevelchej7apcdpz8x4rdlpd5q8h4j4nlujs7lfx4nds2jqe6v2zxvk3qfr7wtu"
+assetname="MeltyLumens"
+cd $assetname/
+export CARDANO_NODE_SOCKET_PATH="/home/narcissa/Desktop/cardano-node/db_main/node.socket"
+address=$(cat payment.addr)
+
+queryresult=$(cardano-cli query utxo --address $address --mainnet)
+echo "${queryresult}" > queryresult.txt
+IFS=' '
+thirdline=`sed -n '3p' queryresult.txt`;
+read -r -a strarr <<< $thirdline
+txhash=${strarr[0]}
+txix=${strarr[1]}
+funds=${strarr[2]}
+policyid=$(cat policy/policyID)
+
+output="0"
+fee="300000"
+adasent="1500000"
+slotnumber="1500000000"
+
+# place this as one of the params if you want metadata.
+# don't forget to build the metadata first!
+#  --metadata-json-file metadata.json \
+
+cardano-cli transaction build-raw \
+  --fee $fee \
+  --tx-in $txhash#$txix \
+  --tx-out $address+$output \
+  --tx-out $winneraddr+$adasent+"$mintamount $policyid.$assetname" \
+  --mint="$mintamount $policyid.$assetname" \
+  --minting-script-file policy/policy.script \
+  --invalid-hereafter $slotnumber \
+  --metadata-json-file metadata.json \
+  --out-file matx.raw
+
+fee=$(cardano-cli transaction calculate-min-fee --tx-body-file matx.raw --tx-in-count 1 --tx-out-count 1 --witness-count 1 --mainnet --protocol-params-file protocol.json | cut -d " " -f1)
+output=$(expr $funds - $fee - $adasent)
+
+cardano-cli transaction build-raw \
+  --fee $fee \
+  --tx-in $txhash#$txix \
+  --tx-out $address+$output \
+  --tx-out $winneraddr+$adasent+"$mintamount $policyid.$assetname" \
+  --mint="$mintamount $policyid.$assetname" \
+  --minting-script-file policy/policy.script \
+  --invalid-hereafter $slotnumber \
+  --metadata-json-file metadata.json \
+  --out-file matx.raw
+
+cardano-cli transaction sign  \
+--signing-key-file payment.skey  \
+--signing-key-file policy/policy.skey  \
+--mainnet --tx-body-file matx.raw  \
+--out-file matx.signed
+
+cardano-cli transaction submit --tx-file matx.signed --mainnet
